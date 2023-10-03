cf4d6ad6670ce0bb4059424d1fd2deba67b135ca
slbabin <>
2023-10-03 14:54:52 +0000

    Create a model for the app

--- shoppinglistapp/models.py
+++ shoppinglistapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
